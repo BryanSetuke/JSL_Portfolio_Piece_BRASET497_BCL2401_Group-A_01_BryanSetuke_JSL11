52a2f62e2004264e53473e8df322cab3a68485a0
BryanSetuke <156883514+BryanSetuke@users.noreply.github.com>
2024-04-24 13:29:45 +0200

    commit

--- index.js
+++ index.js
@@ -75,8 +75,7 @@ function displayBoards(boards) {
 // Filters tasks corresponding to the board name and displays them on the DOM.
 // TASK: Fix Bugs
 function filterAndDisplayTasksByBoard(boardName) {
-  const tasks = getTasks(); // Fetch tasks from a simulated local storage function
-  const filteredTasks = tasks.filter(task => task.board = boardName);
+  const filteredTasks = getTasks().filter(task => task.board === boardName);
 
   // Ensure the column titles are set outside of this function or correctly initialized before this function runs
 
